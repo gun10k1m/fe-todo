8a6dbe9cea975d21453fe7bc6a060cb3ebd7745c
luna10k1m <luna@10k1m.com>
2025-04-03 13:36:01 +0900

    feat: 이메일 유효성 검사 메시지 추가

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,16 +7,27 @@ import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import Link from 'next/link';
+import { useMutation } from '@tanstack/react-query';
+import { createClient } from '@/utils/client';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/hooks/use-toast';
+
+// todo
+// 1. zod 검사 파일 따로 만들기
+// 2. mutation 로직 파일 따로 만들기
 
 export default function SignupPage() {
+  const supabase = createClient();
+  const router = useRouter();
+  const { toast } = useToast();
   interface SignupForm {
     email: string;
     password: string;
     username?: string;
   }
-  //검사 파일 따로 만들기
   const registerSchema = z.object({
-    email: z.string().email().min(1, { message: '이메일을 입력해주세요.' }),
+    email: z.string().min(1, { message: '이메일을 입력해주세요.' }).email('이메일 형식이 올바르지 않습니다.'),
     password: z.string().min(8, { message: '8자 이상 입력해주세요.' }),
     username: z.string(),
   });
@@ -26,12 +37,34 @@ export default function SignupPage() {
     defaultValues: {
       email: '',
       password: '',
+      username: '',
     },
   });
 
   const onSubmit = (data: SignupForm) => {
+    // signup(data);
     console.log(data);
   };
+
+  const { mutate: signup, isPending } = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (data: SignupForm) => {
+      const { error } = await supabase.auth.signUp({ email: data.email, password: data.password });
+      if (error) {
+        throw error;
+      }
+      return data;
+    },
+    onSuccess: () => {
+      toast({
+        title: '회원가입이 완료되었습니다.',
+      });
+      router.push('/');
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
   return (
     <div className="flex justify-center items-center h-screen ">
       <Card className="w-full max-w-md p-10">
@@ -83,11 +116,14 @@ export default function SignupPage() {
                 </FormItem>
               )}
             />
+            <Button type="submit" className="flex justify-center w-full mt-4">
+              회원가입
+            </Button>
           </form>
         </Form>
-        <Button type="submit" className="flex justify-center w-full mt-10">
-          회원가입
-        </Button>
+        <Link href="/login" className="flex justify-center mt-4 text-sm text-gray-500">
+          로그인 하러 가기
+        </Link>
       </Card>
     </div>
   );
