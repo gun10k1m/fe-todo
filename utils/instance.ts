4684fd63981f6c5444720ba8280d8967db21ccf7
doranSeo <doran@10k1m.com>
2025-04-04 17:55:19 +0900

    delete: instance.ts 삭제

--- utils/instance.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-  },
-});
