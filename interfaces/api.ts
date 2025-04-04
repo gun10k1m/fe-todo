ec34d259db38fdfc98c9990ff324c1853158706d
luna10k1m <luna@10k1m.com>
2025-04-04 10:10:21 +0900

    feat: 로그인/회원가입 폴더 합치기

--- interfaces/api.ts
+++ interfaces/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
