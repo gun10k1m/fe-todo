338804a2c486a20c7c632fd2f3abe6798eb00c4d
doranSeo <doran@10k1m.com>
2025-04-06 14:13:30 +0900

    refactor: Detail Dialog에 fech Data 표시

--- components/todos/detailModal.tsx
+++ components/todos/detailModal.tsx
@@ -11,6 +11,7 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { useGetTodoDetail } from '@/queries/todos/queries';
+import { LoaderCircle } from 'lucide-react';
 
 interface TodoDetailModalProps {
   id: number | null;
@@ -22,15 +23,27 @@ export function TodoDetailModal({ id, open, onOpenChange }: TodoDetailModalProps
   const { data, isLoading, error } = useGetTodoDetail(open ? id : null);
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[500px]">
+      <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Todo.title</DialogTitle>
-          <DialogDescription>Todo Descriptioin</DialogDescription>
+          {isLoading ? (
+            <>
+              <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+              로딩 중...
+            </>
+          ) : error ? (
+            <>
+              <DialogTitle>에러 발생</DialogTitle>
+              <DialogDescription>Todo 정보를 불러오는 데 실패했습니다.</DialogDescription>
+            </>
+          ) : (
+            <>
+              <DialogTitle>{data?.title}</DialogTitle>
+              <DialogDescription>{data?.description}</DialogDescription>
+            </>
+          )}
         </DialogHeader>
         <DialogFooter>
-          <Button variant="outline" onClick={() => onOpenChange(false)}>
-            닫기
-          </Button>
+          <Button onClick={() => onOpenChange(false)}>닫기</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
