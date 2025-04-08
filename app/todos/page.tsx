6292444bea804744e0830fe917d54d0b91dff1c6
Luna Yang <luna@10k1m.com>
2025-04-08 13:46:35 +0900

    feat: 띄어쓰기 break-all로 수정

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -142,7 +142,7 @@ function TodoList() {
 
   useEffect(() => {
     setOffset(0);
-  }, [completed, debouncedKeyword, isInfiniteMode]);
+  }, [completed, debouncedKeyword]);
 
   const isLastPage = !isInfiniteMode && offset + LIMIT > paginatedData?.totalCount;
   const isFirstPage = offset === 0;
@@ -203,61 +203,59 @@ function TodoList() {
             <Accordion type="single" collapsible className="space-y-4">
               {(isInfiniteMode ? infiniteData?.pages.flat() : paginatedData)?.map(
                 (todo: TodoProps, index: number, array: TodoProps[]) => (
-                  <>
-                    <AccordionItem
-                      value={todo.id.toString()}
-                      key={todo.id}
-                      ref={index === array.length - 1 ? lastTodoElementRefCallback : null}
-                      className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow"
-                    >
-                      <div className="flex gap-4">
-                        <Checkbox
-                          checked={todo.completed}
-                          onCheckedChange={() => patchCompleted({ id: todo.id, completed: !todo.completed })}
-                          className="mt-6 data-[state=checked]:bg-green-500"
-                        />
-                        <div className="flex-1">
-                          <AccordionTrigger className="text-left">
-                            <div
-                              className={`font-semibold text-lg ${todo.completed ? 'line-through text-gray-400' : ''}`}
-                            >
-                              {todo.title}
-                            </div>
-                          </AccordionTrigger>
-                          <AccordionContent className="text-sm text-muted-foreground mt-2">
-                            <div className="break-words">{todo.description || '설명이 없습니다.'}</div>
-                          </AccordionContent>
-                        </div>
-                        <div className="mt-3">
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button variant="ghost">
-                                <Ellipsis className="h-4 w-4" />
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="flex flex-col items-center">
-                              <DropdownMenuItem
-                                onSelect={() => {
-                                  setDetailOpen(true);
-                                  setSelectedTodoId(todo.id);
-                                }}
-                                className="cursor-pointer w-full flex justify-center"
-                              >
-                                수정
-                              </DropdownMenuItem>
-                              <DropdownMenuSeparator className="w-full" />
-                              <DropdownMenuItem
-                                onSelect={() => handleDeleteClick(todo.id)}
-                                className="cursor-pointer w-full flex justify-center"
-                              >
-                                삭제
-                              </DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </div>
+                  <AccordionItem
+                    value={todo.id.toString()}
+                    key={todo.id}
+                    ref={index === array.length - 1 ? lastTodoElementRefCallback : null}
+                    className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow"
+                  >
+                    <div className="flex gap-4">
+                      <Checkbox
+                        checked={todo.completed}
+                        onCheckedChange={() => patchCompleted({ id: todo.id, completed: !todo.completed })}
+                        className="mt-6 data-[state=checked]:bg-green-500"
+                      />
+                      <div className="flex-1">
+                        <AccordionTrigger className="text-left">
+                          <div
+                            className={`font-semibold text-lg ${todo.completed ? 'line-through text-gray-400' : ''}`}
+                          >
+                            {todo.title}
+                          </div>
+                        </AccordionTrigger>
+                        <AccordionContent className="text-sm text-muted-foreground mt-2">
+                          <div className="break-all">{todo.description || '설명이 없습니다.'}</div>
+                        </AccordionContent>
                       </div>
-                    </AccordionItem>
-                  </>
+                      <div className="mt-3">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button variant="ghost">
+                              <Ellipsis className="h-4 w-4" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent className="flex flex-col items-center">
+                            <DropdownMenuItem
+                              onSelect={() => {
+                                setDetailOpen(true);
+                                setSelectedTodoId(todo.id);
+                              }}
+                              className="cursor-pointer w-full flex justify-center"
+                            >
+                              수정
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator className="w-full" />
+                            <DropdownMenuItem
+                              onSelect={() => handleDeleteClick(todo.id)}
+                              className="cursor-pointer w-full flex justify-center"
+                            >
+                              삭제
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </div>
+                    </div>
+                  </AccordionItem>
                 ),
               )}
             </Accordion>
